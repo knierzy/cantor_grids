932d641e297eec60722f37d8a14279e350847856
knierzy <42247124+knierzy@users.noreply.github.com>
2025-05-07 21:46:54 +0200

    Update ccantor_garnet_subfields.py

--- ccantor_garnet_subfields.py
+++ ccantor_garnet_subfields.py
@@ -149,10 +149,6 @@ def add_rechtecke_mit_farbverlauf(rechtecke, x_offset, spiegeln=False):
 add_rechtecke_mit_farbverlauf(rechtecke, 0)
 
 
-# Load data from  Excel file
-file_path_gilgen = r"C:\Users\wolfgang.knierzinger\Desktop\cantor_anwend\Rohdaten_für_eq_berch\Kompendium_Ö.xlsx"
-df = pd.read_excel(file_path_gilgen, sheet_name='Garn_ex')
-
 # Remove rows with NaN in columns "Unnamed: 1" to "Unnamed: 4"; filter only rows where the sum is >= 98
 df_parameters = df[['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4']].dropna()
 df_parameters = df_parameters[df_parameters.apply(lambda row: row[['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4']].sum() >= 98, axis=1)]
