19d00f5a9d8f333721c47723dac78f9b1e44ff75
knierzy <42247124+knierzy@users.noreply.github.com>
2026-03-28 20:33:18 +0100

    Update c5_3.py

--- c5_3.py
+++ c5_3.py
@@ -658,6 +658,7 @@ print("Points cols: ['Alm','Spe','Pyr','Gro']")
 legend_text = (
     "<span style='font-size:40px; font-weight:bold;'>Garnet Provenance Groups</span><br>"
     "<span style='font-size:28px; font-style:italic;'>Classification based on Mahalanobis distance</span><br><br>"
+    "<span style='font-size:24px;'>Symbols: circles = Mica schist (MS) Pernegg, squares = Linz–Melk Formation</span><br><br>"
 )
 
 for file_path in ordered_hulls:
