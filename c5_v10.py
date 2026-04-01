16108628e70334e2870645b011c6b353219ab729
knierzy <42247124+knierzy@users.noreply.github.com>
2026-04-01 23:26:26 +0200

    Update c5_v10.py

--- c5_v10.py
+++ c5_v10.py
@@ -557,7 +557,7 @@ fig.add_trace(go.Scatter(
         cmax=1,
         coloraxis="coloraxis",
         opacity=0.9,
-        line=dict(width=0)
+        line=dict(width=4)
     ),
     hoverinfo="skip",
     showlegend=False
