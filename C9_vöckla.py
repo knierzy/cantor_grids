6775c65c04125536be809612ab66096d8eee085c
knierzy <42247124+knierzy@users.noreply.github.com>
2026-07-07 11:10:07 +0200

    Update C9_vöckla.py

--- C9_vöckla.py
+++ C9_vöckla.py
@@ -7,7 +7,7 @@
 #   • AWC (Available Water Capacity) displayed as color halo
 #   • Inner points colored by soil texture class
 #   • Export to HTML, PNG (Playwright), and TIFF (400 dpi)
-#   • Plots 107 points from the district Kirchdorf
+#   • district Vöcklabruck
 
 import pandas as pd
 import plotly.graph_objects as go
@@ -1187,60 +1187,6 @@ fig.update_layout(
         font_size=22,
         font_family="Arial"
     )
-)
-# Add a manual annotation pointing to a specific datapoint in the plot
-fig.add_annotation(
-    x=1570,
-    y=6.3,
-
-    ax=-100,
-    ay=-100,
-
-    xref="x",
-    yref="y",
-    axref="pixel",
-    ayref="pixel",
-
-    text=(
-        "<b>"
-        "Location:<br>"
-        "Almuferweg, Pettenbach<br>"
-        "Sand (A): 48.0%<br>"
-        "Silt (B): 40.0%<br>"
-        "Humus (C): 5.0%<br>"
-        "Clay (D): 7.0%<br>"
-        "Class: Loamy Sand<br>"
-        "AWC: 12.57 Vol.%"
-
-    ),
-  showarrow=True,
-    arrowhead=3,
-    arrowsize=1.3,
-    arrowwidth=2,
-    arrowcolor="black",
-
-    font=dict(size=26),
-    bgcolor="rgba(255,255,255,0.9)",
-    bordercolor="black",
-    borderwidth=1,
-    align="left"
-)
-
-# arrow
-fig.add_annotation(
-    x=1080,
-    y=5.25,       
-    ax=1080,
-    ay=5.99,       
-    xref="x",
-    yref="y",
-    axref="x",
-    ayref="y",
-    showarrow=True,
-    arrowhead=3,  
-    arrowsize=1.2,
-    arrowwidth=3,
-    arrowcolor="black"
 )
 
 
